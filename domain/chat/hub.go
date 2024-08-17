4b4437ad212abdc2de7f9557660d5606ab8714ec
kjuiop <copying_y@naver.com>
2024-08-17 13:59:46 +0900

    [#10] feat: 채팅 메서드 로그 추가, 소켓 클로즈 에러 처리

--- domain/chat/hub.go
+++ domain/chat/hub.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"live-chat-server/domain/room"
+	"log/slog"
 )
 
 type Room struct {
@@ -35,15 +36,19 @@ func (r *Room) chatInit() {
 		select {
 		case client := <-r.Join:
 			r.Clients[client] = true
+			slog.Info("join", "client_id", client.userId, "room_id", client.Room.RoomId)
 		case client := <-r.Leave:
 			r.Clients[client] = false
 			close(client.Send)
 			delete(r.Clients, client)
+			slog.Info("leave", "client_id", client.userId, "room_id", client.Room.RoomId)
 		case msg := <-r.Forward:
 			// msg type 에 따른 분기 처리 가능
 			switch msg.Method {
 			case "chat":
 				r.broadcastChat(msg)
+				slog.Info("leave", "send_user_id", msg.SendUserId, "chat_message", msg.Message)
+			default:
 			}
 		}
 	}
