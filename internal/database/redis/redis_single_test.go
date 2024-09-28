529e23c5de4effb71661a23d98ccaa8c241e3ba1
kjuiop <copying_y@naver.com>
2024-09-28 11:39:11 +0900

    [#20] test: redis slow log test code add

--- internal/database/redis/redis_single_test.go
+++ internal/database/redis/redis_single_test.go
@@ -40,3 +40,34 @@ func TestRedisSetGet(t *testing.T) {
 	err = testClient.rdb.Del(ctx, "test_key").Err()
 	assert.NoError(t, err)
 }
+
+func TestRedisSlowLog(t *testing.T) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	originalValue, err := testClient.rdb.ConfigGet(ctx, "slowlog-log-slower-than").Result()
+	assert.NoError(t, err)
+
+	// 모든 명령어를 slow log 기록
+	err = testClient.rdb.ConfigSet(ctx, "slowlog-log-slower-than", "0").Err()
+	assert.NoError(t, err)
+
+	// 1초 sleep 명령어 실행
+	_, err = testClient.rdb.Do(ctx, "DEBUG", "SLEEP", 1).Result() // 1초 Sleep
+	assert.NoError(t, err)
+
+	// 모든 slow log 를 조회
+	slowLogs, err := testClient.rdb.SlowLogGet(ctx, -1).Result()
+	assert.NoError(t, err)
+
+	assert.True(t, len(slowLogs) > 0, "Slowlog 기록이 없습니다.")
+
+	for _, log := range slowLogs {
+		t.Logf("Slowlog ID: %d, Command: %v, Execution Time: %d µs\n",
+			log.ID, log.Args, log.Duration)
+	}
+
+	err = testClient.rdb.ConfigSet(ctx, "slowlog-log-slower-than", originalValue[1].(string)).Err()
+	assert.NoError(t, err)
+}
